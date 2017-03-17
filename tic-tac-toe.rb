56dd2c9eb9cd7a0bd8786ac7a08c86181a32502d
lujanfernaud <lujanfernaud@gmail.com>
2017-03-17 17:38:12 +0000

    Initial commit

---
+++ tic-tac-toe.rb
@@ -0,0 +1,10 @@
+# Project: Tic-tac-toe in Ruby for The Odin Project
+# Author: Luján Fernaud
+#
+# Instructions:
+#
+# Build a tic-tac-toe game on the command line where two human players can play
+# against each other and the board is displayed in between turns.
+#
+# Wikipedia: https://en.wikipedia.org/wiki/Tic-tac-toe
+
