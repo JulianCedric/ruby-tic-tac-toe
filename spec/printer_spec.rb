d7f344df7071a3f75fcaa08bb2796664de2b29e6
lujanfernaud <lujanfernaud@gmail.com>
2017-06-03 22:07:40 +0100

    Add specs for Printer

---
+++ spec/printer_spec.rb
@@ -0,0 +1,20 @@
+describe Printer do
+  let(:game)    { Game.new }
+  let(:board)   { game.board }
+  let(:printer) { board.printer }
+
+  describe "attributes" do
+    it "knows about grid" do
+      expect(printer.grid).to be_a(Hash)
+    end
+  end
+
+  describe "#print_board" do
+    it "prints board" do
+      allow(printer).to receive(:system).with("clear")
+      allow(printer).to receive(:system).with("cls")
+      allow(printer).to receive(:puts).exactly(17).times
+      printer.print_board
+    end
+  end
+end
